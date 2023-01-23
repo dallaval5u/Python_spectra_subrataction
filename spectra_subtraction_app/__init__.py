45d3279190468a56836cecd2110bcf9a3a13102e
Riccardo D <42295487+dallaval5u@users.noreply.github.com>
2023-01-23 15:27:49 +0100

    Update __init__.py

--- spectra_subtraction_app/__init__.py
+++ spectra_subtraction_app/__init__.py
@@ -2,10 +2,11 @@ from spectra_subtraction_app.datahandler import RegionsCollection, Region
 from spectra_subtraction_app.fitter import Fitter, Peak
 from spectra_subtraction_app import helpers, plotter
 
-__appname__ = "SpecQP"
+__appname__ = "spectra_subtraction_app"
 __version__ = "0.1"
-__authors__ = ["Mikhail Shipilin <mikhail.shipilin@gmail.com>"]
-__website__ = "https://github.com/Shipilin/specqp"
+__authors__ = ["Dallavalle Riccardo, dallavallericcardo@outlook.com"]
+__website__ = "https://github.com/dallaval5u/Python_spectra_subtraction_app
+"
 
 __all__ = [
         'datahandler',
