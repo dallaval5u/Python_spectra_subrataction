8ace2906fa4d0937efea9fc263647da21baeab56
Riccardo D <42295487+dallaval5u@users.noreply.github.com>
2023-01-23 15:32:47 +0100

    Update __init__.py

--- spectra_subtraction_app/__init__.py
+++ spectra_subtraction_app/__init__.py
@@ -6,7 +6,7 @@ __appname__ = "spectra_subtraction_app"
 __version__ = "0.1"
 __authors__ = ["Dallavalle Riccardo, dallavallericcardo@outlook.com"]
 __website__ = "https://github.com/dallaval5u/Python_spectra_subtraction_app"
-"
+
 
 __all__ = [
         'datahandler',
